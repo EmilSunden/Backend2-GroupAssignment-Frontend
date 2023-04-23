c2b7ae35c1c083fb61af346b253072098bc561b7
w44t3r <vk.educate@protonmail.ch>
2023-04-23 18:59:55 +0200

    Add Header & Home - Fullpost components to the main, fixed isAuth state && add routes && add styles container.

--- mernsocialmediaapp/src/App.js
+++ mernsocialmediaapp/src/App.js
@@ -1,10 +1,14 @@
-import Navbar from "./components/navbar/Navbar";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Header} from "./components/Header";
+import {Route, Routes} from "react-router-dom";
 import React, {useEffect} from "react";
 import Registration from "./components/Register/Register";
 import Login from "./components/Login/Login";
+import Container from "@mui/material/Container";
+
 import {auth} from "./Action/userAction";
 import {useDispatch, useSelector} from "react-redux";
+import {Home, FullPost} from "./pages";
+
 
 
 
@@ -17,21 +21,25 @@ function App() {
         }
     }, [])
 
+
     const isAuth = useSelector(state => state.user.isAuth)
 
     return (
-        <BrowserRouter>
-            <div className='App'>
-                <Navbar/>
-                <div className="wrap">
-                    {!isAuth && <Routes>
-                        <Route path="/registration" element={<Registration/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                    </Routes>
-                    }
-                </div>
-            </div>
-        </BrowserRouter>
+        <>
+            <Header/>
+            <Container maxWidth="lg">
+                {!isAuth && <Routes>
+                    <Route path="/registration" element={<Registration/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+                }
+                {isAuth && <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/posts/:id" element={<FullPost/>}/>
+                </Routes>
+                }
+            </Container>
+        </>
     );
 }
 
