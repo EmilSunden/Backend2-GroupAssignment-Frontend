f6e06c0d9c6d3ce5824538914b21afd61bc88a44
w44t3r <vk.educate@protonmail.ch>
2023-04-23 19:09:00 +0200

    Add a fullpost component for one post, when we select a post.

---
+++ mernsocialmediaapp/src/pages/FullPost.jsx
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {Post} from "../components/Post";
+import {Index} from "../components/AddComment";
+import {CommentsBlock} from "../components/CommentsBlock";
+import axios from '../axios'
+
+export const FullPost = () => {
+
+    const [data, setObj] = useState()
+    const [isLoading, setLoading] = useState(true)
+    const {id} = useParams()
+
+    useEffect(() => {
+        axios(`/posts/${id}`)
+            .then((res) => {
+                setObj(res.data)
+                setLoading(false)
+                console.log(res)
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+    }, [])
+    if (isLoading) {
+        return <Post isLoading={isLoading} isFullPost/>
+    }
+    return (
+        <>
+            <Post
+                _id={data._id}
+                title={data.title}
+                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                createdAt={data.createdAt}
+                viewsCount={data.view}
+                user={{
+                    avatarUrl:
+                        'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
+                    fullName: data.user.username,
+                }}
+                commentsCount={3}
+                tags={['react', 'fun', 'typescript']}
+                isFullPost
+            >
+                <p>
+                    {data.text}
+                </p>
+            </Post>
+            <CommentsBlock
+                items={[
+                    {
+                        user: {
+                            fullName: "EMILTRYHARDER",
+                            avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+                        },
+                        text: "test Comment",
+                    },
+                    {
+                        user: {
+                            fullName: "JAMES",
+                            avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+                        },
+                        text: "TOP COMMENTS",
+                    },
+                ]}
+                isLoading={false}
+            >
+                <Index/>
+            </CommentsBlock>
+        </>
+    );
+};
