18d00e29603688b0d159a3b7e1a4ba5724637215
w44t3r <vk.educate@protonmail.ch>
2023-04-23 19:11:37 +0200

    import postsReducer to the store

--- mernsocialmediaapp/src/reducer/index.js
+++ mernsocialmediaapp/src/reducer/index.js
@@ -2,10 +2,13 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from "redux-thunk";
 import userReducer from "./userReducer";
+import {postsReducer} from "./slices/posts";
 
 const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    posts:postsReducer
 })
 
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
