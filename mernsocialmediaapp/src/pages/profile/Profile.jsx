95c4a1203c7d4e911d4961b9243416732fb70198
w44t3r <vk.educate@protonmail.ch>
2023-04-24 02:14:27 +0200

    Deleted not used imports, rename i to index, fixed import, changed to export const.

--- mernsocialmediaapp/src/pages/profile/Profile.jsx
+++ mernsocialmediaapp/src/pages/profile/Profile.jsx
@@ -1,20 +1,18 @@
 import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import axios from '../../axios';
-import { UserInfo } from '../../components/UserInfo';
 import ProfileFeed from '../../components/ProfileFeed/ProfileFeed';
 
-const Profile = () => {
+export const Profile = () => {
     const [profile, setProfile] = useState([]);
     const [posts, setPosts] = useState([])
 
-    const { username } = useParams();
+    const {username} = useParams();
 
     const fetchProfile = async () => {
         const response = await axios.get(`/user/${username}`);
-        console.log(response)
         const userData = response.data.user;
-        if (Array.isArray(userData)){
+        if (Array.isArray(userData)) {
             setProfile(userData)
         } else {
             setProfile([userData])
@@ -31,16 +29,14 @@ const Profile = () => {
         fetchPosts();
     }, [])
 
-  return (
-    <div>
-    {profile && profile.map((person, i) => (
-        <div key={i}>
-            <h2>@{person.username}</h2>
+    return (
+        <div>
+            {profile && profile.map((person, index) => (
+                <div key={index}>
+                    <h2>Nickname: {person.username}</h2>
+                </div>
+            ))}
+            {posts && <ProfileFeed posts={posts}/>}
         </div>
-    ))}
-    {posts && <ProfileFeed posts={posts}/>}
-</div>
-  )
-}
-
-export default Profile
+    );
+};
