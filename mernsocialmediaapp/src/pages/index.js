552c492f652f1cba9d8b6773e2625c2842fff6ad
w44t3r <vk.educate@protonmail.ch>
2023-04-24 02:48:20 +0200

    Add imports pages register && Login to the main index imports file

--- mernsocialmediaapp/src/pages/index.js
+++ mernsocialmediaapp/src/pages/index.js
@@ -2,4 +2,6 @@ export { Home } from "./Home";
 export { FullPost } from "./FullPost";
 export { AddPost } from "./AddPost";
 export {Profile} from "./profile/Profile";
+export {Login} from "./Login"
+export {Registration} from "./Registration"
 
