5f2c83ead4049ca52187978ca01bffb72c7c547a
elinhorn <elin.horn@hyperisland.se>
2023-04-25 08:58:04 -0400

    added a function to make user redriect to feed when logged in

--- mernsocialmediaapp/src/pages/Login/index.jsx
+++ mernsocialmediaapp/src/pages/Login/index.jsx
@@ -7,11 +7,18 @@ import Button from "@mui/material/Button";
 import styles from "./Login.module.scss";
 import {useDispatch} from "react-redux";
 import {login} from "../../Action/userAction";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const dispatch = useDispatch()
+    let navigate = useNavigate()
+
+    function handleLoginClick() {
+        dispatch(login(username, password))
+        navigate(`/profile/following/:${username}`) 
+    }
 
   return (
     <Paper classes={{ root: styles.root }}>
@@ -26,7 +33,7 @@ export const Login = () => {
         fullWidth
       />
       <TextField className={styles.field} label="Password" value={password}  onChange={(e)=> setPassword(e.target.value)} fullWidth />
-      <Button size="large" variant="contained" onClick={() => dispatch(login(username, password))} fullWidth>
+      <Button size="large" variant="contained" onClick={handleLoginClick} fullWidth>
         Log In
       </Button>
     </Paper>
