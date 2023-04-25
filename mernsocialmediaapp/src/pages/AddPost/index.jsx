1439408ba4cea66e22e0aa1698270b9306d93ece
w44t3r <vk.educate@protonmail.ch>
2023-04-25 05:03:26 +0200

    Create/Rework AddPost page, now you can create posts, after creating post you will be redirected to it.

--- mernsocialmediaapp/src/pages/AddPost/index.jsx
+++ mernsocialmediaapp/src/pages/AddPost/index.jsx
@@ -2,70 +2,96 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import axios from "../../axios";
 import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
+import {useNavigate} from "react-router-dom";
 
 export const AddPost = () => {
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
+    const navigate = useNavigate()
+    const imageUrl = '';
+    const [text, setText] = React.useState('');
+    const [isConfirmed, setConfirmed] = React.useState(false);
+    const [title, setTitle] = React.useState('');
 
-  const handleChangeFile = () => {};
 
-  const onClickRemoveImage = () => {};
+//for files, if there is a desire to attach them to the backend
+    const handleChangeFile = () => {
+    };
+    const onClickRemoveImage = () => {
+    };
+////////////////////////////////////
 
-  const onChange = React.useCallback((value) => {
-    setValue(value);
-  }, []);
 
-  const options = React.useMemo(
-    () => ({
-      spellChecker: false,
-      maxHeight: '400px',
-      autofocus: true,
-      placeholder: 'Введите текст...',
-      status: false,
-      autosave: {
-        enabled: true,
-        delay: 1000,
-      },
-    }),
-    [],
-  );
+    const onSubmit = async () => {
+        try {
+            const fields = {
+                title,
+                text
+            }
+            const {data} = await axios.post('/posts/create', fields)
+            const id = data._id
+            console.log(id)
+            navigate(`/posts/${id}`)
+        } catch (e) {
+            console.log(e)
+        }
 
-  return (
-    <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
-        Загрузить превью
-      </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
-      {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Удалить
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
-      )}
-      <br />
-      <br />
-      <TextField
-        classes={{ root: styles.title }}
-        variant="standard"
-        placeholder="Заголовок статьи..."
-        fullWidth
-      />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
-      <div className={styles.buttons}>
-        <Button size="large" variant="contained">
-          Опубликовать
-        </Button>
-        <a href="/">
-          <Button size="large">Отмена</Button>
-        </a>
-      </div>
-    </Paper>
-  );
+    }
+
+
+
+    const onChange = React.useCallback((value) => {
+        setText(value);
+    }, []);
+
+    const options = React.useMemo(
+        () => ({
+            spellChecker: true,
+            maxHeight: '400px',
+            autofocus: true,
+            placeholder: 'Enter text...',
+            status: false
+        }),
+        [],
+    );
+
+    return (
+        <Paper style={{padding: 30}}>
+            <Button variant="outlined" size="large">
+                Load preview image
+            </Button>
+            <input type="file" onChange={handleChangeFile} hidden/>
+            {imageUrl && (
+                <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+                    Delete
+                </Button>
+            )}
+            {imageUrl && (
+                <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded"/>
+            )}
+            <br/>
+            <br/>
+            <TextField
+                classes={{root: styles.title}}
+                variant="standard"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Article title..."
+                fullWidth
+            />
+            <TextField classes={{root: styles.tags}} variant="standard" placeholder="Tags" fullWidth/>
+            <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options}/>
+            <div className={styles.buttons}>
+                <Button onClick={onSubmit} size="large" variant="contained">
+                    Publish
+                </Button>
+                <a href="/">
+                    <Button size="large">Cancel</Button>
+                </a>
+            </div>
+        </Paper>
+    );
 };
