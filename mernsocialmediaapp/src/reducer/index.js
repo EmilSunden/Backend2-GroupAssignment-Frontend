29d90ed49b7e542f54b5a359820f321ae4e07ec1
EmilSunden <emilsunjobb@gmail.com>
2023-04-26 23:30:23 +0200

    imported folllow reducer

--- mernsocialmediaapp/src/reducer/index.js
+++ mernsocialmediaapp/src/reducer/index.js
@@ -2,7 +2,8 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from "redux-thunk";
 import userReducer from "./userReducer";
-import {postsReducer, followReducer} from "./slices/posts";
+import {postsReducer} from "./slices/posts";
+import { followReducer } from "./slices/follow";
 
 const rootReducer = combineReducers({
     user: userReducer,
