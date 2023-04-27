57d98c8726f7fff5be6321b14259fe84e26022eb
EmilSunden <emilsunjobb@gmail.com>
2023-04-27 09:48:29 +0200

    Deleted imports

--- mernsocialmediaapp/src/reducer/index.js
+++ mernsocialmediaapp/src/reducer/index.js
@@ -3,12 +3,10 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from "redux-thunk";
 import userReducer from "./userReducer";
 import {postsReducer} from "./slices/posts";
-import { followReducer } from "./slices/follow";
 
 const rootReducer = combineReducers({
     user: userReducer,
     posts:postsReducer,
-    follow: followReducer
 })
 
 
